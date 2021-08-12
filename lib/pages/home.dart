dfe5a21d7c5a33c638a592f43f760a94d45c7a9f
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-08-12 14:11:50 +0530

    update: Added bottom nav in home screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -24,6 +24,46 @@ class InstagramHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: topBar,
+      body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              color: Colors.grey,
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.search,
+              color: Colors.grey,
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.add_box,
+              color: Colors.grey,
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.grey,
+            ),
+            label: "",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person,
+              color: Colors.grey,
+            ),
+            label: "",
+          ),
+        ],
+      ),
     );
   }
 }
