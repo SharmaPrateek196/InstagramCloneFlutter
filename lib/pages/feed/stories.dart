d0a6c6e49490d91824a865beadb658aea035a804
SharmaPrateek196 <sharmaprateek196@gmail.com>
2021-08-17 11:46:16 +0530

    add: story thubnail widget

--- lib/pages/feed/stories.dart
+++ lib/pages/feed/stories.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
+class StoriesPalatte extends StatefulWidget {
+  const StoriesPalatte({Key? key}) : super(key: key);
+
+  @override
+  _StoriesPalatteState createState() => _StoriesPalatteState();
+}
+
+class _StoriesPalatteState extends State<StoriesPalatte> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
 Widget getStoriesHeading() {
   return Container(
     margin: EdgeInsets.all(10),
@@ -17,6 +31,17 @@ Widget getStoriesHeading() {
           style: TextStyle(fontWeight: FontWeight.bold),
         )
       ],
+    ),
+  );
+}
+
+Widget getStoryThumbnail(String url, bool hasSeen) {
+  return Container(
+    width: 80,
+    height: 80,
+    decoration: BoxDecoration(
+      shape: BoxShape.circle,
+      image: DecorationImage(image: NetworkImage(url), fit: BoxFit.fill),
     ),
   );
 }
